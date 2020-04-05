6ae6d8bb3d1b7b59c3c13fe476f658f67d7b76c5
My <vasylnv19@gmail.com>
2020-04-05 22:55:56 +0300

    added method createFigures

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,32 +1,5 @@
-
-/*Необходимо описать задачу с применением принципов ООП.
-Условие тестовой задачи:
-Дано объекты-фигуры следующих видов: квадрат, треугольник, круг, трапеция.
-Каждую фигуру можно нарисовать, получить ее площадь и цвет.
-Также фигуры имеют уникальные методы, например: вернуть радиус, длину гипотенузы, длину стороны и т. д.
-Нам необходимо сгенерировать случайный набор фигур, количество объектов в наборе также заранее неизвестно.
-После генерации массива нужно вывести весь список объектов, которые у нас имеются, например:
-
- Фигура: квадрат, площадь: 25 кв. ед., длина стороны: 5 ед., цвет: синий
- Фигура: треугольник, площадь: 12,5 кв.ед., гипотенуза: 7.1 ед., цвет: желтый*/
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 public class Application {
     public static void main(String[] args) {
-
-        List<Figure> figures = new ArrayList<>();
-        figures.add(new Trapezoid());
-        figures.add(new Circle());
-        figures.add(new Square());
-        figures.add(new Triangle());
-
-        for (Figure figure : figures) {
-            System.out.println(figure.getClass() + " " + figure.square() + " " + figure
-                    .generationRandomColor() );
-        }
+        CreateFigures.createFigures();
     }
-
 }
---
+++ src/main/java/CreateFigures.java
@@ -0,0 +1,30 @@
+
+/*Необходимо описать задачу с применением принципов ООП.
+Условие тестовой задачи:
+Дано объекты-фигуры следующих видов: квадрат, треугольник, круг, трапеция.
+Каждую фигуру можно нарисовать, получить ее площадь и цвет.
+Также фигуры имеют уникальные методы, например: вернуть радиус, длину гипотенузы, длину стороны и т. д.
+Нам необходимо сгенерировать случайный набор фигур, количество объектов в наборе также заранее неизвестно.
+После генерации массива нужно вывести весь список объектов, которые у нас имеются, например:
+
+ Фигура: квадрат, площадь: 25 кв. ед., длина стороны: 5 ед., цвет: синий
+ Фигура: треугольник, площадь: 12,5 кв.ед., гипотенуза: 7.1 ед., цвет: желтый*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateFigures {
+    public static void createFigures() {
+
+        List<Figure> figures = new ArrayList<>();
+        figures.add(new Trapezoid());
+        figures.add(new Circle());
+        figures.add(new Square());
+        figures.add(new Triangle());
+
+        for (Figure figure : figures) {
+            System.out.println("Figure : " + figure.getClass() + ", square - " + figure.square()+ "sq.m," + " color -" + figure
+                    .generationRandomColor());
+        }
+    }
+}

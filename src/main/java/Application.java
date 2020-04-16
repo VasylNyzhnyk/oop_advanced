46dee24cf3149a6d26fa992ba3eadd7a3d80374f
My <vasylnv19@gmail.com>
2020-04-16 14:57:38 +0300

    added method createFigures

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,4 +2,6 @@ public class Application {
     public static void main(String[] args) {
         CreateFigures.createFigures();
     }
+
+
 }
--- src/main/java/Figure.java
+++ src/main/java/Figure.java
@@ -1,18 +1,20 @@
 public abstract class Figure {
     abstract double square();
-    abstract void draw();
-    String generationRandomColor(){
-             int i = (int) (Math.random() * 4);
-             if (i == 0) {
-                 return "red";
-             } else if (i == 1) {
-                 return "orange";
-             } else if (i == 2){
-                 return "blue";
-             }else return "black";
 
-         }
-     }
+    abstract void draw();
+
+    String generationRandomColor() {
+        int i = (int) (Math.random() * 4);
+        if (i == 0) {
+            return "red";
+        } else if (i == 1) {
+            return "orange";
+        } else if (i == 2) {
+            return "blue";
+        } else return "black";
+
+    }
+}
 
 
 
